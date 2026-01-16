8567717047f6d03968fe3612e60974c8f0ef453a
Gilson Nunes <ndevs.studio@outlook.com>
2026-01-15 23:01:36 -0300

    feat: adds database connection on the server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import Fastify, { type FastifyServerOptions } from 'fastify';
+import { getDbPool } from './providers/databaseProvider';
 import { apiRoutes } from './routes/apiRoutes';
 
 export function serverBuild(opts: FastifyServerOptions = {}) {
 	const app = Fastify(opts);
+
+	const dbPool = getDbPool();
+	app.decorateRequest('dbPool', {
+		getter: () => dbPool,
+	});
 
 	app.register(apiRoutes);
 
